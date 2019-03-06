5e95d85703375ad2736dea4f034eb50ec8fe8c77
王小龙 <1036199213@qq.com>
2019-03-06 16:37:48 +0800

    editor

--- gui/src/components/rich-editor.vue
+++ gui/src/components/rich-editor.vue
@@ -1,26 +1,30 @@
 <template>
-    <div ref="editor">
-        {{value}}
+    <div>
+        <div ref="editor">
+            {{value}}
+        </div>
     </div>
 </template>
 <script>
-export default {
-    props:{
-        value:null,
-        options:Object
-    },
-    mounted(){
-        if(typeof Quill ==='function'){
-            const dom = this.$refs.editor;
-            this.quill = new Quill(dom, {
-                 theme: 'snow',
-                 ...this.options
-            });
+    export default {
+        props: {
+            value: null,
+            options: Object
+        },
+        mounted() {
+            if (typeof Quill === 'function') {
+                const editor = this.$refs.editor;
+                this.quill = new Quill(editor, {
+                    theme: 'snow',
+                    ...this.options
+                });
+                this.quill.on('editor-change', () => {
+                    var c = this.quill.container.firstChild.innerHTML;
+                    this.$emit("input",c);
+                });
+            } else {
+                console.error("this component need  Quill.js ,please require it .");
+            }
         }
-        else{
-            console.error("this component need  Quill.js ,please require it .");
-        }
-        
     }
-}
 </script>
